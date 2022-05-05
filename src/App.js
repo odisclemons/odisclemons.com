69f24daee5d889e405333fd8343b159c35231156
Odis Clemons <odis.clemons@gmail.com>
2022-05-05 19:12:33 -0400

    added github to profile photo

--- src/App.js
+++ src/App.js
@@ -39,28 +39,21 @@ function App() {
               <div
                 className="portrait"
                 style={{ backgroundImage: "url(odis.jpg)", padding: "4rem" }}
-                onClick={() =>
-                  window.open(
-                    "https://www.linkedin.com/in/odisclemons",
-                    "_blank"
-                  )
-                }
               >
-                <div className="portfolioInfo">
-                  <div
-                    style={{
-                      display: "flex",
-                      alignItems: "center",
-                      justifyContent: "center",
-                    }}
-                  >
+                <div className="portfolioInfo" style={{ justifyContent: 'space-evenly' }}>
+                  <a href="https://www.linkedin.com/in/odisclemons" target="_blank"><div className="myLinks">
                     <span className="linkedInText">Linked</span>
                     <img
-                      style={{ height: "2.5rem", width: "auto" }}
-                      src="5a22d420c9a5a7.416105621512231968826.png"
+                      src="linkedInLogo.png"
                       alt="linkedIn logo"
                     />
-                  </div>
+                  </div></a>
+                  <a href="https://github.com/odisclemons" target="_blank"><div className="myLinks">
+                    <span className="linkedInText">Github</span>
+                    <svg aria-hidden="true" viewBox="0 0 16 16" version="1.1" width="32" data-view-component="true" style={{ color: 'white' }} >
+                      <path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"></path>
+                    </svg>
+                  </div></a>
                 </div>
               </div>
             </Col>
@@ -68,19 +61,20 @@ function App() {
               lg={6}
               md={12}
               sm={12}
+              id="mainHeader"
               className="jtCol jtCol1"
               style={{
                 justifyContent: "space-between",
               }}
             >
               <h3
-                className="mainHeader"
-                style={{ fontWeight: "bold", textAlign: "center" }}
+                id="siteTitle"
+                style={{ fontWeight: "bold", textAlign: "center", }}
               >
                 OdisClemons.com
               </h3>
               <p
-                className="mainHeader"
+
                 style={{ color: "black", fontWeight: "unset", margin: 0 }}
               >
                 Welcome to my personal website. As you may have guessed, I am
@@ -104,11 +98,18 @@ function App() {
                   <li>Node.js</li>
                   <li>Javascript</li>
                   <li>Express</li>
+                  <li>MongoDB</li>
+                  <li>Mysql</li>
+                  <li>Mongoose</li>
+                  <li>Sequelize</li>
+                  <li>Jest</li>
                   <li>HTML</li>
                   <li>CSS</li>
                   <li>Bootstrap</li>
-                  <li>Jest</li>
+                  <li>Docker</li>
                   <li>Heroku</li>
+                  <li>Linux</li>
+                  <li>Nginx</li>
                 </ul>
               </div>
             </Col>
